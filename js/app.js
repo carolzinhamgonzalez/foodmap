18285c658b84dda3dfdd03167455133c6c7b6da0
Carol Gonzalez <carolzinha.mgonzalez@gmail.com>
2018-08-02 17:45:49 -0300

    segundo dia

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
+// inserção do mapa
 var map;
-
 function initialize() {
     var latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
-
     var options = {
         zoom: 5,
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-
     map = new google.maps.Map(document.getElementById("mapa"), options);
 }
-
 initialize();
+
+// filtrar os restaurantes
+$(document).ready(function()){
+    $('#filter').click(=>{
+      var value = $(this).val();
+      return value;
+    })
+    console.log(value);
+
+    // $( "#filter" ).keyup(function() {
+    //     var value = $( this ).val();
+    //     $( "p" ).text( value );
+    //   })
+    //   .keyup();
+
+}
